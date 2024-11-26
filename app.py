d5d2b28961bbeb398329bb0a22edb2716703c28e
Math-benites <matheus.benitrs@gmail.com>
2024-11-26 12:02:51 -0300

    funcionando 1.0

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect
 import paramiko
+from config import MIKROTIK_HOST
+
 
 app = Flask(__name__)
 app.secret_key = "sua_chave_secreta"  # Substitua por algo seguro
@@ -25,7 +27,7 @@ def execute_ssh_command(host, username, password, command):
 # Rota para exibir o formulário
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', mikrotik_host=MIKROTIK_HOST)
 
 # Rota para enviar o comando
 @app.route('/send_command', methods=['POST'])
@@ -36,15 +38,37 @@ def send_command():
     name = request.form['name']
     user_password = request.form['user_password']
     
+    # Comando a ser enviado via SSH
     command = f"/ppp/secret/add name={name} password={user_password} profile=CLIENTES"
     result = execute_ssh_command(host, username, password, command)
     
     if "Erro" in result or "Exception" in result:
         flash(f"Falha ao enviar comando: {result}", "error")
+        script = None
+        command_status = "Falha ao enviar o comando."
     else:
         flash("Comando enviado com sucesso!", "success")
-    
-    return redirect('/')
+        script = generate_script(name)  # Gera o script com o nome do cliente
+        command_status = f"Comando enviado para {host} com sucesso!"
+
+    return render_template(
+        'index.html',
+        mikrotik_host=host,
+        script=script,
+        command_status=command_status
+    )
+
+
+
+def generate_script(uservpn):
+    try:
+        # Carrega o template do arquivo
+        with open('script_template.txt', 'r') as file:
+            template = file.read()
+        # Substitui o placeholder pelo nome do cliente
+        return template.replace("{uservpn}", uservpn)
+    except FileNotFoundError:
+        return "Erro: Arquivo de template não encontrado."
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,2 @@
+# Configurações da MikroTik
+MIKROTIK_HOST = "192.168.1.16"  # Substitua pelo IP padrão da sua MikroTik
